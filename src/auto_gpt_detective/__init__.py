1fb4d66b26e30ef6015d9e88be1352e9510cca13
Alan Morrison <alan_morrison@taylor.edu>
2023-05-03 19:32:03 -0400

    initial sapling implementation

---
+++ src/auto_gpt_detective/__init__.py
@@ -0,0 +1,113 @@
+"""This is a template for Auto-GPT plugins."""
+import os
+from typing import Any, Dict, List, Optional, Tuple, TypeVar, TypedDict
+from auto_gpt_plugin_template import AutoGPTPluginTemplate
+from .sapling_ai import _sapling_detect
+
+PromptGenerator = TypeVar("PromptGenerator")
+
+
+class Message(TypedDict):
+    role: str
+    content: str
+
+
+class AutoGPTDetective(AutoGPTPluginTemplate):
+    """
+    Detect AI generated text.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._name = "autogpt-detective"
+        self._version = "0.0.1"
+        self._description = "Detect AI generated text."
+        self.load_sapling = (
+                os.getenv("DETECTIVE_ENGINE").lower() == "saplingai"
+                and os.getenv("SAPLINGAI_API_KEY")
+        )
+
+    def can_handle_post_prompt(self) -> bool:
+        return True
+
+    def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
+        if self.load_sapling:
+            prompt.add_command(
+                "Sapling Detect",
+                "sapling_detect",
+                {"query": "<query>"},
+                _sapling_detect,
+            )
+        else:
+            print(
+                "Warning: Detective Engine not loaded. "
+                "Please set DETECTIVE_ENGINE and the appropriate API key environment variables. "
+                "(SAPLINGAI_API_KEY)"
+            )
+
+            return prompt
+
+    def can_handle_pre_command(self) -> bool:
+        return True
+
+    def pre_command(
+            self, command_name: str, arguments: Dict[str, Any]
+    ) -> Tuple[str, Dict[str, Any]]:
+        if command_name == "detect" and self.load_sapling:
+            return "sapling_detect", arguments
+        else:
+            return command_name, arguments
+
+    def can_handle_post_command(self) -> bool:
+        return False
+
+    def post_command(self, command_name: str, response: str) -> str:
+        pass
+
+    def can_handle_on_planning(self) -> bool:
+        return False
+
+    def on_planning(
+            self, prompt: PromptGenerator, messages: List[Message]
+    ) -> Optional[str]:
+        pass
+
+    def can_handle_on_response(self) -> bool:
+        return False
+
+    def on_response(self, response: str, *args, **kwargs) -> str:
+        pass
+
+    def can_handle_post_planning(self) -> bool:
+        return False
+
+    def post_planning(self, response: str) -> str:
+        pass
+
+    def can_handle_pre_instruction(self) -> bool:
+        return False
+
+    def pre_instruction(self, messages: List[Message]) -> List[Message]:
+        pass
+
+    def can_handle_on_instruction(self) -> bool:
+        return False
+
+    def on_instruction(self, messages: List[Message]) -> Optional[str]:
+        pass
+
+    def can_handle_post_instruction(self) -> bool:
+        return False
+
+    def post_instruction(self, response: str) -> str:
+        pass
+
+    def can_handle_chat_completion(
+            self, messages: Dict[Any, Any], model: str, temperature: float, max_tokens: int
+    ) -> bool:
+        return False
+
+    def handle_chat_completion(
+            self, messages: List[Message], model: str, temperature: float, max_tokens: int
+    ) -> str:
+        pass
---
+++ src/auto_gpt_detective/sapling_ai.py
@@ -0,0 +1,40 @@
+import os
+import requests
+import json
+
+
+def _sapling_detect(text: str, sent_scores: bool = False) -> str:
+    api_key = os.getenv("SAPLINGAI_API_KEY")
+    post_endpoint = os.getenv("SAPLINGAI_POST_ENDPOINT")
+    if post_endpoint is None:
+        post_endpoint = "https://api.sapling.ai/api/v1/aidetect"
+
+    body = {
+        "key": api_key,
+        "text": text,
+        "sent_scores": sent_scores
+    }
+
+    response = requests.post(post_endpoint, json=body)
+    response.raise_for_status()
+
+    results = response.json()
+    return json.dumps(results, ensure_ascii=False, indent=4)
+
+
+class SaplingAIDetector:
+    def __init__(self, api_key: str, post_endpoint: str = 'https://api.sapling.ai/api/v1/aidetect'):
+        self.api_key = api_key
+        self.post_endpoint = post_endpoint
+
+    def detect(self, text: str, sent_scores: bool = False) -> dict:
+        # post request
+        body = {
+            'key': self.api_key,
+            'text': text,
+            'sent_scores': sent_scores
+        }
+
+        response = requests.post(self.post_endpoint, json=body)
+        return response.json()
+
